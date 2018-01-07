badbb8e1f5f6371c0e2f008ad5fa8c0bfc839a98
TuDatTr <tran.tuan-dat@mailbox.org>
2018-01-07 12:17:26 +0100

    cleanup

--- .emacs.d/snippets/latex-mode/auto/.emacs.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- ".emacs"
- (lambda ()
-   (TeX-run-style-hooks
-    "INCL/settings"
-    "{INCL/tu}"))
- :latex)
-
--- .scripts/auto/a.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "a"
- (lambda ()
-   (TeX-run-style-hooks
-    "INCL/settings"
-    "{INCL/tu}"))
- :latex)
-
