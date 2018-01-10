3fa8a813f01c93f2b33f26835f61fae1ead43359
TuDatTr <tran.tuan-dat@mailbox.org>
2018-01-10 06:33:52 +0100

    .

--- emacs/.emacs.d/config.el
+++ emacs/.emacs.d/config.el
@@ -1,3 +1,31 @@
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives 
+             '("melpa" . "https://melpa.org/packages/")
+)
+
+;; Add themes inside "~/.emacs.d/themes/" to loaded themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'forest-blue t)
+
+(custom-set-faces
+ '(font-latex-bold-face ((t (:foreground "brightcyan"))))
+ '(font-latex-sedate-face ((t (:foreground "brightcyan"))))
+ '(font-lock-comment-face ((t (:foreground "color-142"))))
+ '(font-lock-doc-face ((t (:foreground "color-83"))))
+ '(font-lock-keyword-face ((t (:foreground "brightcyan" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "green"))))
+ '(highlight-indentation-face ((t (:background "green")))))
+
+
+
+(require 'smartparens-config)
+
+(require 'tex)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-close-quote "")
+(setq TeX-open-quote "")
+(setq-default TeX-Master nil)
+
+(add-hook 'org-mode-hook 'smartparens-strict-mode)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,2 +1,20 @@
 (package-initialize)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (rainbow-delimiters smartparens auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-latex-bold-face ((t (:foreground "brightcyan"))))
+ '(font-latex-sedate-face ((t (:foreground "brightcyan"))))
+ '(font-lock-comment-face ((t (:foreground "color-142"))))
+ '(font-lock-doc-face ((t (:foreground "color-83"))))
+ '(font-lock-keyword-face ((t (:foreground "brightcyan" :weight bold))))
+ '(font-lock-type-face ((t (:foreground "green"))))
+ '(highlight-indentation-face ((t (:background "green")))))
