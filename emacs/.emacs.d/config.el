3ffe49c436d0ecf0b3e278bb6af801b082693c34
TuDatTr <tran.tuan-dat@mailbox.org>
2018-01-13 15:29:33 +0100

    Removed elpa packages, need to find a way to auto install

--- emacs/.emacs.d/config.el
+++ emacs/.emacs.d/config.el
@@ -1,31 +1,110 @@
 
 (require 'package)
 (add-to-list 'package-archives 
-             '("melpa" . "https://melpa.org/packages/")
-)
+    '("melpa" . "http://melpa.org/packages/"))
 
-;; Add themes inside "~/.emacs.d/themes/" to loaded themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'forest-blue t)
 
-(custom-set-faces
- '(font-latex-bold-face ((t (:foreground "brightcyan"))))
- '(font-latex-sedate-face ((t (:foreground "brightcyan"))))
- '(font-lock-comment-face ((t (:foreground "color-142"))))
- '(font-lock-doc-face ((t (:foreground "color-83"))))
- '(font-lock-keyword-face ((t (:foreground "brightcyan" :weight bold))))
- '(font-lock-type-face ((t (:foreground "green"))))
- '(highlight-indentation-face ((t (:background "green")))))
+(defun copy-to-xclipboard(arg)
+  (interactive "P")
+  (cond
+   ((not (use-region-p))
+    (message "Nothing to yank to X-clipboard"))
+   ((and (not (display-graphic-p))
+         (/= 0 (shell-command-on-region
+                (region-beginning) (region-end) "xsel -i -b")))
+    (error "Is program `xsel' installed?"))
+   (t
+    (when (display-graphic-p)
+      (call-interactively 'clipboard-kill-ring-save))
+    (message "Yanked region to X-clipboard")
+    (deactivate-mark))))
 
+(defun cut-to-xclipboard(arg)
+  (interactive "P")
+  (cond
+   ((not (use-region-p))
+    (message "Nothing to yank to X-clipboard"))
+   ((and (not (display-graphic-p))
+         (/= 0 (shell-command-on-region
+                (region-beginning) (region-end) "xsel -i -b")))
+    (error "Is program `xsel' installed?"))
+   (t
+    (when (display-graphic-p)
+    (call-interactively 'clipboard-kill-ring-save))
+    (message "Yanked region to X-clipboard")
+    (kill-region  (region-beginning) (region-end))
+    (deactivate-mark))))
 
+(defun paste-from-xclipboard()
+  "Uses shell command `xsel -o' to paste from x-clipboard. With
+    one prefix arg, pastes from X-PRIMARY, and with two prefix args,
+    pastes from X-SECONDARY."
+  (interactive)
+  (if (display-graphic-p)
+      (clipboard-yank)
+    (let*
+        ((opt (prefix-numeric-value current-prefix-arg))
+         (opt (cond
+               ((=  1 opt) "b")
+               ((=  4 opt) "p")
+               ((= 16 opt) "s"))))
+      (insert (shell-command-to-string (concat "xsel -o -" opt))))))
 
-(require 'smartparens-config)
+(defun back-to-indentation-or-beginning () 
+"Go back to indentation, if at indentation go to beginning of line"
+(interactive)
+ (if (= (point) (progn (back-to-indentation) (point)))
+     (beginning-of-line)))
+
+(menu-bar-mode -1)
+
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
 (require 'tex)
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
-(setq TeX-close-quote "")
-(setq TeX-open-quote "")
-(setq-default TeX-Master nil)
+(setq-default TeX-master nil)
 
-(add-hook 'org-mode-hook 'smartparens-strict-mode)
+(require 'ox-twbs)
+
+(add-hook 'org-mode-hook
+      '(lambda ()
+         (delete '("\\.pdf\\'" . default) org-file-apps)
+         (add-to-list 'org-file-apps '("\\.pdf\\'" . "evince %s"))))
+
+(add-hook 'org-mode-hook 
+      '(lambda () 
+         (setq org-src-fontify-natively t)))
+
+(ivy-mode 1)
+
+(require 'paren)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode +1)
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "#333333")
+(set-face-foreground 'highlight nil)
+
+(require 'yasnippet)
+(setq yas-snippet-dirs
+  '("~/.emacs.d/snippets/"))
+(yas-global-mode 1)
+
+(defvar my-keys-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-a") 'back-to-indentation-or-beginning)
+    (define-key map (kbd "C-c M-w") 'copy-to-xclipboard)
+    (define-key map (kbd "C-c C-w") 'cut-to-xclipboard)
+    (define-key map (kbd "C-c M-y") 'paste-from-xclipboard)
+    map)
+  "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  :init-value t
+  :lighter " my-keys")
+
+(my-keys-minor-mode 1)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,16 +5,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (rainbow-delimiters smartparens auctex))))
+ '(package-selected-packages
+   (quote
+    (anaconda-mode rainbow-mode ox-twbs google-this auctex smartparens eclim flycheck rainbow-delimiters forest-blue-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-latex-bold-face ((t (:foreground "brightcyan"))))
- '(font-latex-sedate-face ((t (:foreground "brightcyan"))))
- '(font-lock-comment-face ((t (:foreground "color-142"))))
- '(font-lock-doc-face ((t (:foreground "color-83"))))
- '(font-lock-keyword-face ((t (:foreground "brightcyan" :weight bold))))
- '(font-lock-type-face ((t (:foreground "green"))))
- '(highlight-indentation-face ((t (:background "green")))))
+ )
