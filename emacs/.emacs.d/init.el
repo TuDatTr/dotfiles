b87f98634275edc673c4a17b74c16f723272b6db
TuDatTr <tran.tuan-dat@mailbox.org>
2020-10-24 06:55:37 +0200

    Testing out theme magic and emojis in md/org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,10 +7,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(moe-theme org-bullets ivy-prescient prescient powerline-evil evil use-package)))
+   '(theme-magic use-package ox-twbs org-bullets no-littering moe-theme ivy-prescient evil-collection diminish counsel)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-hide ((t (:foreground "#000000")))))
