61f54b288ab92379d0ac719e522f1231a484e30b
TuDatTr <tuan-dat.tran@tudattr.dev>
2022-04-08 13:16:17 +0200

    Changes on Fri 08 Apr 2022 01:16:17 PM CEST
    Signed-off-by: TuDatTr <tuan-dat.tran@tudattr.dev>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,6 +5,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(ledger-reports
+   '(("privatausgaben" "%(binary) -f %(ledger-file) reg Girokonto and not expr 'any(account =~ /Unternehmenskosten|Versicherung|Wohnungskosten|Gemeinschaftskonto|LiviaE/)' and not @Umbuchung and not @Kleingeld --period 'this month'")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)")))
  '(org-export-backends '(ascii html latex md odt))
  '(package-selected-packages
    '(auctex-latexmk flycheck-rust ansible yasnippet use-package theme-magic rainbow-mode rainbow-delimiters ox-twbs ox-reveal ox-hugo org-bullets no-littering moe-theme magit ivy-prescient htmlize god-mode go-mode flycheck evil-collection diminish counsel company auctex)))
